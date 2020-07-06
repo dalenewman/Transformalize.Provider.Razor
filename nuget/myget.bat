2348c486f1a37faf85801e1c96c8b10f39a00fa0
Dale Newman <dalenewman@gmail.com>
2020-07-06 17:29:18 -0400

    Update Nuget Push Script

--- nuget/myget.bat
+++ nuget/myget.bat
@@ -1,5 +1,5 @@
-REM nuget push "c:\temp\modules\Transformalize.Provider.Razor.0.8.15-beta.nupkg" -source https://www.myget.org/F/transformalize/api/v3/index.json
-REM nuget push "c:\temp\modules\Transformalize.Provider.Razor.Autofac.0.8.15-beta.nupkg" -source https://www.myget.org/F/transformalize/api/v3/index.json
-REM nuget push "c:\temp\modules\Transformalize.Transform.Razor.0.8.15-beta.nupkg" -source https://www.myget.org/F/transformalize/api/v3/index.json
-REM nuget push "c:\temp\modules\Transformalize.Transform.Razor.Autofac.0.8.15-beta.nupkg" -source https://www.myget.org/F/transformalize/api/v3/index.json
+nuget push "c:\temp\modules\Transformalize.Provider.Razor.0.8.16-beta.nupkg" -source https://www.myget.org/F/transformalize/api/v3/index.json
+nuget push "c:\temp\modules\Transformalize.Provider.Razor.Autofac.0.8.16-beta.nupkg" -source https://www.myget.org/F/transformalize/api/v3/index.json
+nuget push "c:\temp\modules\Transformalize.Transform.Razor.0.8.16-beta.nupkg" -source https://www.myget.org/F/transformalize/api/v3/index.json
+nuget push "c:\temp\modules\Transformalize.Transform.Razor.Autofac.0.8.16-beta.nupkg" -source https://www.myget.org/F/transformalize/api/v3/index.json
 
