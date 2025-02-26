1a61e6a6ea2c22d0fdb1e9fc97340552a19c5f71
Dale Newman <dalenewman@gmail.com>
2025-02-25 21:41:21 -0500

    - update dependencies

--- nuget/myget.bat
+++ nuget/myget.bat
@@ -1,5 +1,9 @@
-nuget push "c:\temp\modules\Transformalize.Provider.Razor.0.10.1-beta.nupkg" -source https://www.myget.org/F/transformalize/api/v3/index.json
-nuget push "c:\temp\modules\Transformalize.Provider.Razor.Autofac.0.10.1-beta.nupkg" -source https://www.myget.org/F/transformalize/api/v3/index.json
-nuget push "c:\temp\modules\Transformalize.Transform.Razor.0.10.1-beta.nupkg" -source https://www.myget.org/F/transformalize/api/v3/index.json
-nuget push "c:\temp\modules\Transformalize.Transform.Razor.Autofac.0.10.1-beta.nupkg" -source https://www.myget.org/F/transformalize/api/v3/index.json
+nuget pack Transformalize.Provider.Razor.nuspec -OutputDirectory "c:\temp\modules"
+nuget pack Transformalize.Provider.Razor.Autofac.nuspec -OutputDirectory "c:\temp\modules"
+nuget pack Transformalize.Transform.Razor.nuspec -OutputDirectory "c:\temp\modules"
+nuget pack Transformalize.Transform.Razor.Autofac.nuspec -OutputDirectory "c:\temp\modules"
 
+nuget push "c:\temp\modules\Transformalize.Provider.Razor.0.11.1-beta.nupkg" -source https://www.myget.org/F/transformalize/api/v3/index.json
+nuget push "c:\temp\modules\Transformalize.Provider.Razor.Autofac.0.11.1-beta.nupkg" -source https://www.myget.org/F/transformalize/api/v3/index.json
+nuget push "c:\temp\modules\Transformalize.Transform.Razor.0.11.1-beta.nupkg" -source https://www.myget.org/F/transformalize/api/v3/index.json
+nuget push "c:\temp\modules\Transformalize.Transform.Razor.Autofac.0.11.1-beta.nupkg" -source https://www.myget.org/F/transformalize/api/v3/index.json
